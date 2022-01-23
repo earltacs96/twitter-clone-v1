d50147ff61b02514e8ee28a7a2a6621354c6db50
earltacs <earltacogdoy@gmail.com>
2022-01-23 22:28:07 +0800

    Initial Commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,147 @@
+<main>
+	<div class="body">
+		<div class="sidebar">
+			<div class="sidebar-content">
+				<div class="icon">
+					<img src="https://img.icons8.com/color/48/000000/twitter--v1.png" alt="twitter" />
+				</div>
+				<div class="links">
+					<a class="link font-bold" href="/">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-7 w-7 mr-4"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+						>
+							<path
+								d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
+							/>
+						</svg>
+						Home</a
+					>
+					<a class="link" href="/">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-7 w-7 mr-4"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="1"
+								d="M7 20l4-16m2 16l4-16M6 9h14M4 15h14"
+							/>
+						</svg>
+						Explore</a
+					>
+					<a class="link" href="/"
+						><svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-7 w-7 mr-4"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="1"
+								d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+							/>
+						</svg>
+						Notification</a
+					>
+					<a class="link" href="/"
+						><svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-7 w-7 mr-4"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="1"
+								d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+							/>
+						</svg>
+						Messages</a
+					>
+					<a class="link" href="/"
+						><svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-7 w-7 mr-4"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="1"
+								d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+							/>
+						</svg>
+						Bookmarks</a
+					>
+					<a class="link" href="/">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-7 w-7 mr-4"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="1"
+								d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
+							/>
+						</svg>
+						Lists</a
+					>
+					<a class="link" href="/">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-7 w-7 mr-4"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="1"
+								d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+							/>
+						</svg>
+						Profile</a
+					>
+					<a class="link" href="/">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-7 w-7 mr-4"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="1"
+								d="M8 12h.01M12 12h.01M16 12h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+							/>
+						</svg>
+						More</a
+					>
+                    <button class="bg-blue-400 hover:bg-blue-500 text-white text-xl font-bold w-4/6 rounded-full py-4 px-2">Tweet</button>
+				</div>
+			</div>
+		</div>
+		<div class="content">s</div>
+	</div>
+</main>
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {},
+		fontFamily: {
+			'apple': ['apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', 'Helvetica', 'Arial', 'sans-serif']
+		}
+	},
+
+	plugins: []
+};
+
+module.exports = config;
