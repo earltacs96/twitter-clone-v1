70005ad78ee027d8f916ab85978539321aa800dd
earltacs <syntacs.2019@gmail.com>
2022-01-24 20:43:57 +0800

    Added design for center

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -138,10 +138,132 @@
 						</svg>
 						More</a
 					>
-                    <button class="bg-blue-400 hover:bg-blue-500 text-white text-xl font-bold w-4/6 rounded-full py-4 px-2">Tweet</button>
+					<button
+						class="bg-blue-400 hover:bg-blue-500 text-white text-xl font-bold w-4/6 rounded-full py-4 px-2"
+						>Tweet</button
+					>
 				</div>
 			</div>
 		</div>
-		<div class="content">s</div>
+		<div class="content">
+			<div class="center">
+				<div class="flex flex-row items-center justify-between">
+					<h1 class="text-2xl font-bold">Home</h1>
+					<div>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
+							/>
+						</svg>
+					</div>
+				</div>
+				<div class="flex flex-col">
+					<div class="flex flex-row">
+						<div class="bg-gray-300 p-2 rounded-full mr-2">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="h-10 w-10 text-gray-400"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</div>
+						<div class="flex flex-col w-full">
+							<div class="border-b mb-1">
+								<input
+									placeholder="What's Happening?"
+									class="text-xl border-0 active:border-0 w-full h-full"
+								/>
+							</div>
+							<div class="flex flex-row items-center justify-between">
+								<div class="flex flex-row">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										class="h-6 w-6 text-blue-500"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke="currentColor"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="1"
+											d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+										/>
+									</svg>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										class="h-6 w-6 text-blue-500"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke="currentColor"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="1"
+											d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+										/>
+									</svg>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										class="h-6 w-6 text-blue-500"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke="currentColor"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="1"
+											d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+										/>
+									</svg>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										class="h-6 w-6 text-blue-500"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke="currentColor"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="1"
+											d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+										/>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="1"
+											d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+										/>
+									</svg>
+								</div>
+								<button
+									class="bg-blue-400 hover:bg-blue-500 text-white text-sm font-bold w-1/6 rounded-full py-2 px-1"
+									>Tweet</button
+								>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="side" />
+		</div>
 	</div>
 </main>
